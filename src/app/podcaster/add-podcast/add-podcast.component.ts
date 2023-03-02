88bd349ef8b5248bba7ae38c61f790657b790338
Amine Mesbahi <mesbahiamine886@gmail.com>
2023-03-02 10:58:19 +0100

    make changes on the add podcast component

--- src/app/podcaster/add-podcast/add-podcast.component.ts
+++ src/app/podcaster/add-podcast/add-podcast.component.ts
@@ -1,4 +1,7 @@
+import { PodcastService } from './../services/podcast.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-add-podcast',
@@ -7,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddPodcastComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formbuilder:FormBuilder,private podcastService:PodcastService) { }
+  addPodcastForm!:FormGroup;
+  sendingRequest:boolean = false;
+
+
 
   ngOnInit(): void {
+    this.addPodcastForm = this.formbuilder.group({
+      "title":this.formbuilder.control(''),
+      "desciption":this.formbuilder.control(''),
+      "FileAudio":null,
+      "picture":null,
+    })
   }
+
+  selectPic(event:any){
+    const file = event.target.files[0];
+    this.addPodcastForm.get('picture')?.setValue(file);
+  }
+  
+  selectAudio(event:any){
+    const file = event.target.files[0];
+    this.addPodcastForm.get('FileAudio')?.setValue(file);
+  }
+
+  addPodcast(){
+    this.sendingRequest = true
+    this.podcastService.addPodcast(this.addPodcastForm).subscribe({
+      next:(re)=>{
+        console.log(re)
+      },
+      error:(err)=>{
+        this.sendingRequest = false
+        console.log(err)
+      },
+      complete:()=>{
+        this.sendingRequest = false
+      }
+    });
+  }
+
 
 }
