40e610c0341c1a6af5a2f4a250a4621c6218d386
Amine Mesbahi <mesbahiamine886@gmail.com>
2023-03-05 14:03:01 +0100

    make changes on the discover component that make it dynamic

--- src/app/home/pages/discover/discover.component.ts
+++ src/app/home/pages/discover/discover.component.ts
@@ -1,3 +1,5 @@
+import { Podcast } from './../../../podcaster/model/Podcast.model';
+import { PodcastService } from './../../../podcaster/services/podcast.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./discover.component.scss']
 })
 export class DiscoverComponent implements OnInit {
-
-  constructor() { }
+  recentPodcasts!:Podcast[];
+  constructor(private podcastService:PodcastService) { }
 
   ngOnInit(): void {
+    this.getRecentPordcasts()
+  }
+
+
+
+  getRecentPordcasts(){
+    this.podcastService.getPodcasts(0,5).subscribe({
+      next:(res)=>{
+        this.recentPodcasts = res
+      },
+      error:(err)=>{
+        console.log(err)
+      },
+    })
   }
 
 }
