57785d79bbeacc7972ea91fca79a14848e5c4acc
Amine Mesbahi <mesbahiamine886@gmail.com>
2023-03-05 12:36:57 +0100

    add the podcast and podcaster interfaces

---
+++ src/app/podcaster/model/Podcaster.model.ts
@@ -0,0 +1,10 @@
+interface Podcaster{
+    id: number|null,
+    email: string|null,
+    name: string|null,
+    phoneNumber: number|null,
+    bio: string|null,
+    profilePic: string|null,
+    linkedinProfile: string|null,
+    verified: boolean
+}
