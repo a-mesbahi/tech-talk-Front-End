e7648c699c9c2ed86841d74cdf6b678428de0a49
Amine Mesbahi <mesbahiamine886@gmail.com>
2023-03-05 14:24:06 +0100

    add the get method to podcasts with pagination

--- src/app/podcaster/services/podcast.service.ts
+++ src/app/podcaster/services/podcast.service.ts
@@ -2,12 +2,13 @@ import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { Podcast } from '../model/Podcast.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PodcastService {
-  private ADD_PODCAST_ENDPOINT = "podcast-service/podcasts";
+  private PODCAST_ENDPOINT = "podcast-service/podcasts";
   constructor(private httpClient:HttpClient) { }
 
 
@@ -18,8 +19,12 @@ export class PodcastService {
         console.log(control?.value)
         podcastFormData.append(key, control?.value);
       })
-      return this.httpClient.post(this.ADD_PODCAST_ENDPOINT,podcastFormData);
+      return this.httpClient.post(this.PODCAST_ENDPOINT,podcastFormData);
   }
 
+
+  getPodcasts(page:Number, size:Number):Observable<Podcast[]>{
+    return this.httpClient.get<Podcast[]>(`${this.PODCAST_ENDPOINT}?page=${page}&size=${size}`)
+  }
 
 }
