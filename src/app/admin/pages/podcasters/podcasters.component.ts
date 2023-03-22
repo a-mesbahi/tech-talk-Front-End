eef52adeaf2705f1eae6b8edf0498b31f9fa7756
Amine Mesbahi <mesbahiamine886@gmail.com>
2023-03-22 20:34:17 +0000

    add the filter and the verification of the podcaster

--- src/app/admin/pages/podcasters/podcasters.component.ts
+++ src/app/admin/pages/podcasters/podcasters.component.ts
@@ -1,3 +1,4 @@
+import { UserServiceService } from './../../../auth/service/user-service.service';
 import { Podcaster } from './../../../podcaster/model/Podcaster.model';
 import { PodcasterService } from './../../../podcaster/services/podcaster.service';
 import { Component, OnInit } from '@angular/core';
@@ -10,36 +11,89 @@ import { environment } from 'src/environments/environment';
 })
 export class PodcastersComponent implements OnInit {
 
-  constructor(private podcasterService:PodcasterService) { }
+  constructor(private podcasterService:PodcasterService,private userService:UserServiceService) { }
   url:string = environment.fireBaseUrl
   podcasters!:Podcaster[];
   page:number = 0 ;
   size:number = 6;
   openAction:boolean=false;
-
+  openFilter:boolean=false;
+  filterValue:string="All";
+  showConfirmation:boolean=false;
   podcastersIdsForAction:number[] = [];
+  theUpdateVerfied:number = 0;
 
 
   ngOnInit(): void {
-    this.podcasterService.getAllpodcaster(this.page, this.size).subscribe({
-      next:(res:Podcaster[])=>{
-        this.podcasters = res
-        console.log(res)
-      },
-      error:()=>{
-        
-      }
-    })
+    this.resetPodcaster()
   }
-
-
 
   addPodcasterToAction(id:number|null){
     if(id!=null ) {
       if(this.podcastersIdsForAction.includes(id)) this.podcastersIdsForAction = this.podcastersIdsForAction.filter(item => item !== id)
       else this.podcastersIdsForAction.push(id)
     }
-    console.log(this.podcastersIdsForAction)
+  }
+
+
+  action(action:number){
+    this.theUpdateVerfied = action
+    this.showConfirmation = true
+  }
+
+  confirm(){
+    this.podcasterService.updateVerefied(this.theUpdateVerfied,this.podcastersIdsForAction).subscribe({
+      next:(res)=>{
+          this.showConfirmation=false;
+          this.openAction=false;
+          console.log(this.filterValue=="All","-".repeat(100))
+          if(this.filterValue!="All"){
+            this.podcasters = this.podcasters.filter(item=> !this.podcastersIdsForAction.includes(item.id!))
+          }else{
+            this.podcasters.forEach(podcast => {
+              if(this.podcastersIdsForAction.includes(podcast.id!)){
+                  podcast.verified=this.theUpdateVerfied
+              }
+            });
+          }
+      },
+      error:()=>{
+        this.userService.logout()
+      }
+    })
+  }
+
+  getPodcasters(verified:number,filterValue:string){
+    this.openFilter=false;
+    this.filterValue=filterValue;
+    this.podcasterService.getPodcastersWithVerified(this.page,this.size,verified).subscribe({
+      next:(res:Podcaster[])=>{
+        this.podcasters = res
+        console.log(res)
+      },
+      error:()=>{
+        this.userService.logout()
+      }
+    })
+  }
+
+  resetPodcaster(){
+    this.openFilter=false;
+    this.filterValue="All";
+    this.podcasterService.getAllpodcaster(this.page, this.size).subscribe({
+      next:(res:Podcaster[])=>{
+        this.podcasters = res
+        console.log(res)
+      },
+      error:()=>{
+        this.userService.logout()
+      }
+    })
+  }
+
+
+  searchPodcast(){
+    
   }
 
 }
